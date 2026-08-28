cd300ff318146a66eb97a54911491cf415387e40
Youssef <youssefshaban982022@gmail.com>
2026-08-28 20:39:56 +0300

    Working on 2 Dice Royale

--- app/src/main/java/com/youssefshaaban/diceroller/MainActivity.kt
+++ app/src/main/java/com/youssefshaaban/diceroller/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.youssefshaaban.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val image: ImageView = findViewById(R.id.dice_iv)
+        val imageTwo: ImageView = findViewById(R.id.two_dice_iv)
+        val btn: Button = findViewById(R.id.roll_btn)
+
+        btn.setOnClickListener {
+            val random = (1..6).random()
+            val randomTwo = (1..6).random()
+            when (randomTwo) {
+                1 -> imageTwo.setImageResource(R.drawable.dice_1)
+                2 -> imageTwo.setImageResource(R.drawable.dice_2)
+                3 -> imageTwo.setImageResource(R.drawable.dice_3)
+                4 -> imageTwo.setImageResource(R.drawable.dice_4)
+                5 -> imageTwo.setImageResource(R.drawable.dice_5)
+                6 -> imageTwo.setImageResource(R.drawable.dice_6)
+
+            }
+
+            when (random) {
+                1 -> image.setImageResource(R.drawable.dice_1)
+                2 -> image.setImageResource(R.drawable.dice_2)
+                3 -> image.setImageResource(R.drawable.dice_3)
+                4 -> image.setImageResource(R.drawable.dice_4)
+                5 -> image.setImageResource(R.drawable.dice_5)
+                6 -> image.setImageResource(R.drawable.dice_6)
+            }
+
+        }
     }
+
 }
